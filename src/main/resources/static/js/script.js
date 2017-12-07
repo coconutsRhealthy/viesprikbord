1156c69b9fc2e663fc3d90f83034c234989f3b2c
Lennart Popma <lennart.popma@ordina.nl>
2017-12-07 16:09:02 +0100

    Changes to preview image before you post imagelink, so that you can flip it if necessary

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,6 +5,7 @@ mainApp.controller('prikbordController', function($scope, $http) {
     $scope.imageUrlToPost;
     $scope.imageLinks;
     $scope.lightBoxImageUrl;
+    $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(0deg); -moz-transform: rotate(0deg); -o-transform: rotate(0deg); -ms-transform: rotate(0deg); transform: rotate(0deg);"
 
     $http.post('/getImageLinks').success(function(data) {
         $scope.imageLinks = data;
@@ -18,5 +19,17 @@ mainApp.controller('prikbordController', function($scope, $http) {
 
     $scope.lightboxFunction = function(imageUrl) {
         $scope.lightBoxImageUrl = imageUrl;
+    }
+
+    $scope.flip = function() {
+        if($scope.style.includes("(0deg)")) {
+            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(90deg); -o-transform: rotate(90deg); -ms-transform: rotate(90deg); transform: rotate(90deg);"
+        } else if($scope.style.includes("(90deg)")) {
+            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(180deg); -o-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);"
+        } else if($scope.style.includes("(180deg)")) {
+            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(270deg); -o-transform: rotate(270deg); -ms-transform: rotate(270deg); transform: rotate(270deg);"
+        } else if($scope.style.includes("(270deg)")) {
+            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(0deg); -o-transform: rotate(0deg); -ms-transform: rotate(0deg); transform: rotate(0deg);"
+        }
     }
 });
