440dec84202e9d4ae11ce11e2aa3cd4248416975
LennartMac <lennartpopma@hotmail.com>
2017-12-11 22:25:57 +0100

    Frontend work. Supermarkt page, image preview, rotation and sizing fix, etc

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,10 +2,13 @@ var mainApp = angular.module("mainApp", []);
 
 mainApp.controller('prikbordController', function($scope, $http) {
 
-    $scope.imageUrlToPost;
+    $scope.imageUrlToPost = '';
     $scope.imageLinks;
     $scope.lightBoxImageUrl;
-    $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(0deg); -moz-transform: rotate(0deg); -o-transform: rotate(0deg); -ms-transform: rotate(0deg); transform: rotate(0deg);"
+    $scope.style = "object-fit: cover; width: 200px; height: 200px; -webkit-transform: rotate(0deg); -moz-transform: rotate(0deg); -o-transform: rotate(0deg); -ms-transform: rotate(0deg); transform: rotate(0deg);"
+    $scope.superMarkets = ["AH Helmholtzstraat"];
+    $scope.selectedSupermarket = "AH Helmholtzstraat";
+    $scope.showPreviewImage = false;
 
     $http.post('/getImageLinks').success(function(data) {
         $scope.imageLinks = data;
@@ -23,13 +26,17 @@ mainApp.controller('prikbordController', function($scope, $http) {
 
     $scope.flip = function() {
         if($scope.style.includes("(0deg)")) {
-            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(90deg); -o-transform: rotate(90deg); -ms-transform: rotate(90deg); transform: rotate(90deg);"
+            $scope.style = "object-fit: cover; width: 200px; height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(90deg); -o-transform: rotate(90deg); -ms-transform: rotate(90deg); transform: rotate(90deg);"
         } else if($scope.style.includes("(90deg)")) {
-            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(180deg); -o-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);"
+            $scope.style = "object-fit: cover; width: 200px; height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(180deg); -o-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);"
         } else if($scope.style.includes("(180deg)")) {
-            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(270deg); -o-transform: rotate(270deg); -ms-transform: rotate(270deg); transform: rotate(270deg);"
+            $scope.style = "object-fit: cover; width: 200px; height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(270deg); -o-transform: rotate(270deg); -ms-transform: rotate(270deg); transform: rotate(270deg);"
         } else if($scope.style.includes("(270deg)")) {
-            $scope.style = "max-width: 200px; max-height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(0deg); -o-transform: rotate(0deg); -ms-transform: rotate(0deg); transform: rotate(0deg);"
+            $scope.style = "object-fit: cover; width: 200px; height: 200px; -webkit-transform: rotate(deg); -moz-transform: rotate(0deg); -o-transform: rotate(0deg); -ms-transform: rotate(0deg); transform: rotate(0deg);"
         }
+    }
+
+    $scope.showPreview = function() {
+        $scope.showPreviewImage = true;
     }
 });
